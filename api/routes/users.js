9f57796a4951ea62883a468efe11e20a8b3d98b9
BrunoLucas21 <brunolucasafreire@gmail.com>
2023-05-15 17:05:24 -0300

    atualizando dados da tela de forms

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addUser, deleteUser, getUsers, updateUser } from '../controller/user'
+import { addUser, deleteUser, getUsers, updateUser } from '../controller/user.js'
 
 const router = express.Router()
 
